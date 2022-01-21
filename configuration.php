8608ee3d13c89fe032f8ad54057b6f798b12f003
Klau <klaudiusmiskinis@gmail.com>
2022-01-21 18:53:44 +0100

    Eliminando conection y creando configuration

--- configuration.php
+++ configuration.php
@@ -1,7 +1,6 @@
-<?php
+<?php 
     require __DIR__.'/vendor/autoload.php';
     use Symfony\Component\Dotenv\Dotenv;
     $dotenv = new Dotenv();
-    $dotenv->load('.env');
-    $dotenv->load(__DIR__.'/.env', __DIR__.'/.env');
+    $dotenv -> load(__DIR__.'/.env');
 ?>
