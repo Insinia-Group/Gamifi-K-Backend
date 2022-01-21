b43eddc078bb8048b1c0f30f0034280bf15cb353
Klau <klaudiusmiskinis@gmail.com>
2022-01-21 17:57:23 +0100

    Creando clase Database con metodos y constructor vacio

---
+++ class/Database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database {
+    /* CONSTRUCTOR */
+    function __construct()
+    {
+        
+    }
+
+    public function connection() {
+        require 'configuration.php';
+        echo getenv('DB_HOST').' '.getenv('DB_USERNAME').' '.getenv('DB_PASSWORD').' '.getenv('DB_DATABASE');
+        $mysql = new mysqli(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_DATABASE'));
+        $mysql -> set_charset("utf8");
+        $res = $mysql -> query("SELECT * FROM test");
+        while($f = $res->fetch_object()){
+            echo $f->nombre.' <br/>';
+        }
+    }
+}
+
+?>
