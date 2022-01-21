36773d0cada84ccc62438d204f058c700090f1fc
Klau <klaudiusmiskinis@gmail.com>
2022-01-21 16:59:46 +0100

    Arreglado fallos, ya carga correctamente.

--- conection.php
+++ conection.php
@@ -1,7 +1,11 @@
 <?php
+    require __DIR__.'/vendor/autoload.php';
     use Symfony\Component\Dotenv\Dotenv;
     $dotenv = new Dotenv();
-    $dotenv->load('../.env');
-    $dotenv->load(__DIR__.'/.env', __DIR__.'/.env.dev');
-
+    $dotenv->load('.env');
+    $dotenv->load(__DIR__.'/.env', __DIR__.'/.env');
+    echo getenv("APP_NAME");
+    echo getenv("DB_NAME");
+    echo getenv("DB_USER");
+    echo getenv("DB_PASSWORD");
 ?>
