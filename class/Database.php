46cc860a9f0ba12bb927dc71d5908ec3854c5d16
Klau <klaudiusmiskinis@gmail.com>
2022-01-24 18:54:10 +0100

    Añadiendo try catch y funcionamiento de la conexion

--- class/Database.php
+++ class/Database.php
@@ -20,17 +20,14 @@ class Database {
         define("DB_USERNAME", getenv('DB_USERNAME'));
         define("DB_PASSWORD", getenv('DB_PASSWORD'));
         define("DB_DATABASE", getenv('DB_DATABASE'));
-        $this -> mysql = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
-        if ($this -> mysql -> connect_errno) {
-            echo "Error con la BD";
-            exit();
-        } else {
-            $asd = $this -> mysql -> query('Select * FROM test');
-            while ($fila = $asd->fetch_assoc()) {
-                echo $fila['testNum'] .' '. $fila['testString'].'<br>';
-            }
-        }
+        try {
+            $this -> mysql = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+            if ($this -> mysql -> connect_errno) {
+                exit();
+            };
+        } catch (Exception $error) {
+            return $error;
+        };
     }
 }
 
