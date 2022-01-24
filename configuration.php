de7d12e74bdce7b2ddde0d645dd46c981e92a720
Klau <klaudiusmiskinis@gmail.com>
2022-01-24 18:54:36 +0100

    Creando token

--- configuration.php
+++ configuration.php
@@ -1,6 +1,17 @@
 <?php 
     require __DIR__.'/vendor/autoload.php';
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
     use Symfony\Component\Dotenv\Dotenv;
     $dotenv = new Dotenv();
     $dotenv -> load(__DIR__.'/.env');
+    $secret = getenv('DB_HOST');
+    $token = array(
+        "iss" => "http://example.org",
+        "aud" => "http://example.com",
+        "iat" => 1356999524,
+        "nbf" => 1357000000
+    );
+    $jwt = JWT::encode($token, $secret, 'HS256');
+    $decoded = JWT::decode($jwt, new Key(getenv('DB_HOST'), 'HS256'));
 ?>
