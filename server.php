d4d66c636e89898eeee0c1b56056e6543b77d511
Klau <klaudiusmiskinis@gmail.com>
2022-01-24 19:52:24 +0100

    Eliminando comentarios

--- server.php
+++ server.php
@@ -2,7 +2,7 @@
     if (isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        header('Access-Control-Max-Age: 86400');
     }
     
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
