994b6cbfb0de635a5ca1f238c2fb4335598802dd
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-25 20:19:11 +0100

    Creando test para pruebas

--- test.php
+++ test.php
@@ -1,5 +1,25 @@
 <?php 
     require 'class/Database.php';
-    $test = new Database();
-    $test -> connection();
+    require 'class/Auth.php';
+    require 'configuration.php';
+    // $test = new Database();
+    // $test -> connection();
+    $hashedSecret = password_hash(getenv('JWT_TOKEN_SECRET'), PASSWORD_BCRYPT);
+    if (password_verify(getenv('JWT_TOKEN_SECRET'), $hashedSecret)) {
+        echo 'true<br>';
+    } else {
+        echo 'false<br>';
+    }
+    
+    $usuario  = 'eduardo';
+    $password = '123456';
+    if($usuario === 'eduardo' && $password === '123456')
+    {
+        echo $a = Auth::SignIn([
+            'id' => 1,
+            'name' => 'Edu'
+        ]);
+
+    }
+
 ?>
