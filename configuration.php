adee14eb3cf763750c721c5fdfd595d570ddb858
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-25 20:19:02 +0100

    Cambios y pruebas para el funcionamiento de JWT

--- configuration.php
+++ configuration.php
@@ -1,17 +1,17 @@
 <?php 
     require __DIR__.'/vendor/autoload.php';
-    use Firebase\JWT\JWT;
     use Firebase\JWT\Key;
+    use Firebase\JWT\JWT;
     use Symfony\Component\Dotenv\Dotenv;
     $dotenv = new Dotenv();
     $dotenv -> load(__DIR__.'/.env');
-    $secret = getenv('DB_HOST');
     $token = array(
-        "iss" => "http://example.org",
-        "aud" => "http://example.com",
         "iat" => 1356999524,
-        "nbf" => 1357000000
+        "nbf" => 1357000000,
+        "user" => 1,
+        "admin" => 'false'
     );
-    $jwt = JWT::encode($token, $secret, 'HS256');
-    $decoded = JWT::decode($jwt, new Key(getenv('DB_HOST'), 'HS256'));
+    $hashedSecret = password_hash(getenv('JWT_TOKEN_SECRET'), PASSWORD_BCRYPT);
+    $jwt = JWT::encode($token, $hashedSecret, 'HS256');
+    $decoded = JWT::decode($jwt, new Key($hashedSecret, 'HS256'));
 ?>
