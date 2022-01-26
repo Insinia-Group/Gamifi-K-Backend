412cd4624609a0a3db7f21a6f554f6132b4a8839
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-26 17:36:11 +0100

    Añadiendo php

--- server.php
+++ server.php
@@ -1,4 +1,4 @@
-<?
+<?php
     if (isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
@@ -9,10 +9,8 @@
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
         exit(0);
     }
  
