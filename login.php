6eb09f922183668e176968a529572ee9ff0be85e
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-26 20:15:51 +0100

    Commit temporal

--- login.php
+++ login.php
@@ -1,7 +1,20 @@
 <?php 
     require 'configuration.php';
     include_once('class/Database.php');
-    $databaseCon = new Database();
-    $databaseCon -> connection();
-    $databaseCon -> getTest();
+    include_once('class/Router.php');
+    $database = new Database();
+    $database -> connection();
+
+    $router = new Router;
+    // How GET requests will be defined
+    $router->get('/some/route', function($request) {
+        // The $request argument of the callback 
+        // will contain information about the request
+        return "Content";
+    });
+    // How POST requests will be defined
+    $router->post('/some/route', function($request) {
+        // How to get data from request body
+        $body = $request->getBody();
+    });
 ?>
