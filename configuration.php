50fb244c1ff291b704a42ee78cecf7ae9dec86e3
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-26 17:35:47 +0100

    Comentarios

--- configuration.php
+++ configuration.php
@@ -1,8 +1,14 @@
 <?php 
+    /* CONFIGURATION */
+    /* Requires */
     require __DIR__.'/vendor/autoload.php';
+
+    /* Uses */
     use Firebase\JWT\Key;
     use Firebase\JWT\JWT;
     use Symfony\Component\Dotenv\Dotenv;
+
+    /* Init classes*/
     $dotenv = new Dotenv();
     $dotenv -> load(__DIR__.'/.env');
 ?>
