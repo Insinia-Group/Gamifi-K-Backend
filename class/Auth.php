47a3206fff757f248fb7027ef02cccd6a4318708
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-26 18:16:34 +0100

    Cambiando funcionamiento y eliminando constructor y atributos.

--- class/Auth.php
+++ class/Auth.php
@@ -1,49 +1,42 @@
 <?php
-
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
- class Auth {
-    private static $secret;
-
-    function __construct() {
-        self::$secret = getenv('JWT_TOKEN_SECRET');
-    }
-
-    public static function decodeToken($token)
-    {
-        return JWT::decode($token, new Key(self::$secret, 'HS256'));
-    }
-
-    public static function SignIn($data) 
-    {
-        $time = time();
-
-        $token = array(
-            'exp' => $time + (60 * 60 * 24 * 1),
-            'aud' => self::Aud(),
-            'data' => $data
-        );
-        echo self::$secret.'<br>';
-        return JWT::encode($token, self::$secret, 'HS256');
-    }
-
-    private static function Aud()
-    {
-        $aud = '';
-
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $aud = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $aud = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $aud = $_SERVER['REMOTE_ADDR'];
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
+    
+    class Auth {
+        private static function secret()
+        {
+            return getenv('JWT_TOKEN_SECRET');
         }
 
-        $aud .= @$_SERVER['HTTP_USER_AGENT'];
-        $aud .= gethostname();
+        public static function decodeToken($token)
+        {
+            return JWT::decode($token, new Key(self::secret(), 'HS256'));
+        }
 
-        return sha1($aud);
+        public static function signIn($data) 
+        {
+            $time = time();
+            $token = array(
+                'exp' => $time + (60 * 60 * 24 * 1),
+                'aud' => self::Aud(),
+                'data' => $data
+            );
+            return JWT::encode($token, self::secret(), 'HS256');
+        }
+
+        private static function Aud()
+        {
+            $aud = '';
+            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+                $aud = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $aud = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } else {
+                $aud = $_SERVER['REMOTE_ADDR'];
+            }
+            $aud .= @$_SERVER['HTTP_USER_AGENT'];
+            $aud .= gethostname();
+            return sha1($aud);
+        }
     }
- }
 ?>
