8a4c96565c52ca7c7234ef17aea5eeb686371b56
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-26 18:28:03 +0100

    Eliminando clases

--- configuration.php
+++ configuration.php
@@ -2,7 +2,5 @@
     /* CONFIGURATION */
     /* Requires */
     require __DIR__.'/vendor/autoload.php';
-    require 'class/Database.php';
-    require 'class/Auth.php';
     include 'dotenv.php';
 ?>
