d6582932c1a7e4247e6bfc03ffb0d5d10963acde
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-26 17:25:35 +0100

    Eliminando hash de contraseña

--- configuration.php
+++ configuration.php
@@ -5,13 +5,4 @@
     use Symfony\Component\Dotenv\Dotenv;
     $dotenv = new Dotenv();
     $dotenv -> load(__DIR__.'/.env');
-    $token = array(
-        "iat" => 1356999524,
-        "nbf" => 1357000000,
-        "user" => 1,
-        "admin" => 'false'
-    );
-    $hashedSecret = password_hash(getenv('JWT_TOKEN_SECRET'), PASSWORD_BCRYPT);
-    $jwt = JWT::encode($token, $hashedSecret, 'HS256');
-    $decoded = JWT::decode($jwt, new Key($hashedSecret, 'HS256'));
 ?>
