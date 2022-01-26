ba3be9f66b4f4ae0d3ff53f841a6a2cd6ee77c73
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-26 18:18:07 +0100

    Cambiando las inicializaciones

--- configuration.php
+++ configuration.php
@@ -2,13 +2,7 @@
     /* CONFIGURATION */
     /* Requires */
     require __DIR__.'/vendor/autoload.php';
-
-    /* Uses */
-    use Firebase\JWT\Key;
-    use Firebase\JWT\JWT;
-    use Symfony\Component\Dotenv\Dotenv;
-
-    /* Init classes*/
-    $dotenv = new Dotenv();
-    $dotenv -> load(__DIR__.'/.env');
+    require 'class/Database.php';
+    require 'class/Auth.php';
+    include 'dotenv.php';
 ?>
