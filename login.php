454906e3478ea301214e525826ae33ce24e5cd8a
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-27 20:09:40 +0100

    Incluyendo distintas clases y archivos para el correcto funcionamiento

--- login.php
+++ login.php
@@ -1,20 +1,37 @@
 <?php 
-    require 'configuration.php';
+    include_once('configuration.php');
+    include_once('class/Request.php');
     include_once('class/Database.php');
     include_once('class/Router.php');
+    include_once('class/Auth.php');
     $database = new Database();
     $database -> connection();
 
-    $router = new Router;
-    // How GET requests will be defined
-    $router->get('/some/route', function($request) {
-        // The $request argument of the callback 
-        // will contain information about the request
-        return "Content";
+    $router = new Router(new Request);
+
+    $router->get('/', function($request) {
+        return 'Estas en /';
     });
-    // How POST requests will be defined
-    $router->post('/some/route', function($request) {
-        // How to get data from request body
-        $body = $request->getBody();
+
+    $router->post('/login', function($request) {
+
+        AUTH::signIn([
+            "email" => 'paco',
+            "password" => '123'
+        ]);
+
+        $inputJSON = file_get_contents('php://input');
+        $input = json_decode($inputJSON, TRUE);
+        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        fwrite($myfile, implode($input));
+        return json_encode(json_decode($inputJSON, TRUE));
+    });
+
+    $router->post('/asd', function($request) {
+        $inputJSON = file_get_contents('php://input');
+        $input = json_decode($inputJSON, TRUE);
+        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        fwrite($myfile, strval(json_encode($input, true)));
+        return json_encode($input, true);
     });
 ?>
