0374b56f593551a16edd49b4254cf0d8a55f6cc7
Klau <73123094+klaudiusmiskinis@users.noreply.github.com>
2022-02-01 12:44:59 +0100

    Cambiando localizacion de los Header

--- configuration.php
+++ configuration.php
@@ -3,4 +3,7 @@
     /* Requires */
     require __DIR__.'/vendor/autoload.php';
     include 'dotenv.php';
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php 
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     include_once('configuration.php');
     include_once('class/Request.php');
     include_once('class/Database.php');
