6d7b8face0f469efb9562dc428fd8346dd642ec5
Klau <klaudiusmiskinis@gmail.com>
2022-02-02 18:49:27 +0100

    Eliminando distintos requires

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php 
-    require_once("{$_SERVER['DOCUMENT_ROOT']}/class/Router.php");
-    require_once('class/Auth.php');
-    require_once('class/Database.php');
     require_once('class/Router.php');
-    require_once('configuration.php');
 
     /* GETs */
-    get('/status', 'get/status.php');
+    post('/status', 'get/status.php');
 
     /* POSTs */
     post('/login', 'views/login.php');
