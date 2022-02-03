72da23344e8b89f8af814f32898433d96d50f2b6
Klau <klaudiusmiskinis@gmail.com>
2022-02-03 18:33:17 +0100

    Llamando al método login para devolver un jwt

--- post/login.php
+++ post/login.php
@@ -1,2 +1,7 @@
 <?php  
+    include_once('class/Database.php');
+    $validation = parse();
+    $database = new Database();
+    $database -> connection();
+    $database -> login($validation -> email, $validation -> password);
     
