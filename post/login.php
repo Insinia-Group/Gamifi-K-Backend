e10bfd1d923cda9fde8dbcb1c09e0763c23ad200
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-13 18:13:48 +0100

    Añadiendo funcionamiento en caso correcto del login

--- post/login.php
+++ post/login.php
@@ -3,6 +3,14 @@
     $validation = parse();
     $database = new Database();
     $database -> connection();
-    $response = $database -> login($validation -> email, $validation -> password);
-    // header('Auth: ' . $response -> token);
-    print_r(json_encode($response));
+    try {
+        foreach($validation as $prop => $val) {
+            unset($validation->{$prop});
+            $validation->{trim($prop)} = trim($val);
+        }
+        $response = $database -> login($validation -> email, $validation -> password);
+        print_r(json_encode($response));
+    } catch (Exception $error) {
+        print_r(json_encode($database -> responseError(403, 'Invalid data')));
+    }
+    
