68bec3474943faee1664f1832611f088d7dca801
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2022-02-22 19:44:40 +0100

    No se puede enviar token i file

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
-<?php 
-    require_once('class/Router.php');
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Access-Control-Expose-Headers: Authorization');
-    require_once('class/Router.php');
+<?php
+require_once('class/Router.php');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Expose-Headers: Authorization');
 
+/* GETs */
+get('/status', 'get/status.php');
+get('/users', 'get/users.php');
 
-    /* GETs */
-    get('/status', 'get/status.php');
-    get('/users', 'get/users.php');
-
-    /* POSTs */
-    post('/login', 'post/login.php');
-    post('/singup', 'post/signup.php');
-    post('/register', 'post/register.php');
-    post('/uploadAvatar', 'post/uploadAvatar.php');
+/* POSTs */
+post('/login', 'post/login.php');
+post('/singup', 'post/signup.php');
+post('/register', 'post/register.php');
+post('/uploadAvatar', 'post/uploadAvatar.php');
--- post/uploadAvatar.php
+++ post/uploadAvatar.php
@@ -4,11 +4,14 @@ $validation = parse();
 $database = new Database();
 $database->connection();
 try {
-    $type = $_FILES['file']['type'];
-    $foto = $_FILES['file']['tmp_name'];
-    $data = file_get_contents($foto);
-    $fotoFinal = 'data:' . $type . ';base64,' . base64_encode($data);
+    if (isset($_FILES['file'])) {
+        $type = $_FILES['file']['type'];
+        $foto = $_FILES['file']['tmp_name'];
+        $data = file_get_contents($foto);
+        $fotoFinal = 'data:' . $type . ';base64,' . base64_encode($data);
+    }
     $header = apache_request_headers();
+    print_r(json_encode($header));
 } catch (Exception $error) {
     print_r(json_encode($database->responseError(403, 'Invalid data')));
 }
