4b2cbb56c16c4a35081f984d0d12588534dd61fa
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2022-02-22 18:58:43 +0100

    Obtenemos token del header

--- post/uploadAvatar.php
+++ post/uploadAvatar.php
@@ -4,14 +4,15 @@
     $database = new Database();
     $database -> connection();
     try {
-        $type = $_FILES['file']['type'];
-        $foto=$_FILES['file']['tmp_name'];
-        $data = file_get_contents($foto);
-
-        $fotoFinal='data:'.$type.';base64,'.base64_encode($data);
-      
-        print_r(json_encode($fotoFinal));
+     //   $type = $_FILES['file']['type'];
+       // $foto=$_FILES['file']['tmp_name'];
+       // $data = file_get_contents($foto);
+        $header = apache_request_headers();
+        //$fotoFinal='data:'.$type.';base64,'.base64_encode($data);
+        print_r(json_encode($header['Authorization']));
     } catch (Exception $error) {
         print_r(json_encode($database -> responseError(403, 'Invalid data')));
     }
+
+    
     
