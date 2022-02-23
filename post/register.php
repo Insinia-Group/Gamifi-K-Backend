80a7f81b2407d6e18d0a781be9122208b1835d84
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2022-02-23 19:29:54 +0100

    token interception

--- post/register.php
+++ post/register.php
@@ -4,7 +4,7 @@
     $database = new Database();
     $database->connection();
     try {
-        $response = $database->register($validation->nick, $validation->userName, $validation->lastUserName, $validation->email, $validation->description, $validation->password, $validation->dateBirth, $validation->avatar, $validation->role, $validation->dateJoined, $validation->status);
+        $response = $database->register($validation->nick, $validation->userName, $validation->lastUserName, $validation->email, $validation->description, $validation->password, $validation->dateBirth, $validation->role, $validation->dateJoined, $validation->status);
     } catch (Exception $error) {
         print_r(json_encode($database->responseError(403, 'Invalid data')));
     }
