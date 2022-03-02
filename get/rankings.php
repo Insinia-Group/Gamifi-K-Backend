e30a26034f8a1d766e1fb162fe04ee272a99ad1d
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2022-03-02 18:49:38 +0100

    Obtener rankings dependiendo si eres usuario o admin

--- get/rankings.php
+++ get/rankings.php
@@ -4,14 +4,17 @@ include_once('class/Auth.php');
 $database = new Database();
 $database->connection();
 try {
-    $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDYyMzg0OTcsImF1ZCI6ImVmYWVjMWY2YjE4YmYyYjc3ZTRmN2I0YjA2OWUwOTRlODJhYjI3NDUiLCJkYXRhIjpbImFhYUBhYWEuYWFhIl19.2fYWGhojWAzXG8DWoMGPjj4GsSLJdXQbM8riiXtjvSc';
+    $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDYzMjk0NzcsImF1ZCI6ImVmYWVjMWY2YjE4YmYyYjc3ZTRmN2I0YjA2OWUwOTRlODJhYjI3NDUiLCJkYXRhIjpbImNhcmFAY2FyYS5jYXJhIiwyMV19.jGHITmLoqbwf_jEH4IiIcKCoPERBcHhWRY4P-pouwVU';
     $decoded = AUTH::decodeToken($token);
     $email = $decoded->data[0];
+    $id = $decoded->data[1];
     $timeExpiration = $decoded->exp;
     $isExpired = AUTH::isExpired($timeExpiration);
     $isAdmin = $database->isAdmin($email);
     if (!$isExpired && $isAdmin) {
         json_encode($database->getRankings());
+    } else if (!$isExpired && !$isAdmin) {
+        json_encode($database->getRankingByUser($id));
     } else if (AUTH::isExpired($timeExpiration)) {
         print_r(json_encode($database->responseError(403, 'Your token access is expired.')));
     } else if (!$isAdmin) {
