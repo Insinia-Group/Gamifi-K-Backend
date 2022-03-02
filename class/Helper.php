c1ce81e7a71f3f25ad4574f04a502fd2623ec95a
Klau <klaudiusmiskinis@gmail.com>
2022-03-02 18:51:59 +0100

    Creando funcion para getter del token de los headers del cliente.

--- class/Helper.php
+++ class/Helper.php
@@ -1,8 +1,19 @@
 <?php
 
+/**
+ * Da formato correcto a un blob corrupto.
+ */
 function fixingBlob($blob)
 {
     $blob = chunk_split(base64_encode($blob));
     $blob = str_ireplace(array("\r", "\n", "\\/", '\r', '\n', '\\/'), '', $blob);
     return $blob;
+}
+
+/**
+ * Devuelve el token de la request del cliente.
+ */
+function getClientToken() {
+    $tokens = apache_request_headers();
+    return $tokens['Authorization'];
 }
