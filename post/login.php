24c64ddd06dcad8db03eba815ed75ebeae8b784f
Klau <klaudiusmiskinis@gmail.com>
2022-03-02 18:53:03 +0100

    Eliminando returns

--- post/login.php
+++ post/login.php
@@ -1,16 +1,12 @@
 <?php
 include_once('class/Database.php');
 include_once('class/Auth.php');
-
 $validation = parse();
 $database = new Database();
 $database->connection();
 try {
     $response = $database->login($validation->email, $validation->password);
     header("Authorization:" . json_encode($response));
-    print_r(json_encode($response));
-    $responses = Auth::decodeToken($response);
-    print_r($responses);
 } catch (Exception $error) {
     print_r(json_encode($database->responseError(403, 'Invalid data')));
 }
