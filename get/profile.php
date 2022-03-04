c6039f1a929512c16ef4fd37bd13cf05646c9d95
Klau <klaudiusmiskinis@gmail.com>
2022-03-04 20:03:06 +0100

    llamando funcion getProfile

--- get/profile.php
+++ get/profile.php
@@ -1,10 +1,11 @@
 <?php
 include_once('class/Database.php');
 include_once('class/Auth.php');
+include_once('class/Helper.php');
 $database = new Database();
 $database->connection();
 try {
-    $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDYzMzEyOTUsImF1ZCI6ImVmYWVjMWY2YjE4YmYyYjc3ZTRmN2I0YjA2OWUwOTRlODJhYjI3NDUiLCJkYXRhIjpbImFhYUBhYWEuYWFhIiwxOF19.YjZSWjvh3oz_xQwpM9yQbgaxI3XIDN2xNMRHRL651nE";
+    $token = getClientToken();
     $decoded = AUTH::decodeToken($token);
     $email = $decoded->data[0];
     $id = $decoded->data[1];
@@ -12,7 +13,7 @@ try {
     $isExpired = AUTH::isExpired($timeExpiration);
     $isAdmin = $database->isAdmin($email);
     if (!$isExpired && $isAdmin) {
-        json_encode($database->getProfile($id));
+        $database->getProfile($id);
     } else if (AUTH::isExpired($timeExpiration)) {
         print_r(json_encode($database->responseError(403, 'Your token access is expired.')));
     } else if (!$isAdmin) {
