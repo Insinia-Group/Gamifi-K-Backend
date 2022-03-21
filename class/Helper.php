baaca2337ea155639f844956dab5b3440d4b8d5a
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2022-03-21 16:16:04 +0100

    Aclarando codigo

--- class/Helper.php
+++ class/Helper.php
@@ -4,7 +4,7 @@
  * Da formato correcto a un blob corrupto.
  */
 function fixingBlob($blob)
-{
+{  
     $blob = chunk_split(base64_encode($blob));
     $blob = str_ireplace(array("\r", "\n", "\\/", '\r', '\n', '\\/'), '', $blob);
     return $blob;
