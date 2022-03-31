acb26a5534473cc96d6de344e73b0c94fb16d371
Klau <klaudiusmiskinis@gmail.com>
2022-03-31 18:37:41 +0200

    Cambiando respuesta de login

--- post/login.php
+++ post/login.php
@@ -7,10 +7,7 @@ $database->connection();
 try {
     $token = $database->login($validation->email, $validation->password);
     if ($token) {
-        $response = new stdClass();
-        $response->isAuthenticated = true;
         header("Authorization:" . json_encode($token));
-        print_r(json_encode($response));
     }
 } catch (Exception $error) {
     print_r(json_encode($database->responseError(403, 'Invalid data')));
