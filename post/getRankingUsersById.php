b3ee5353a39ec5e5f8ee1ecbc7d11ced84a495b3
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-12 12:46:06 +0200

    Creando funcionamiento de los distintos archivos que maneja la API

--- post/getRankingUsersById.php
+++ post/getRankingUsersById.php
@@ -8,7 +8,7 @@ $token = getClientToken();
 $decoded = AUTH::decodeToken($token);
 $id = $decoded->data[1];
 try {
-    $database->insertUsersToRanking($validation->users, $validation->idRanking);
+    $database->getUsersByRanking($validation -> idRanking);
 } catch (Exception $error) {
-    print_r(json_encode($database->responseError(403, 'Invalid data')));
+    print_r(json_encode($database->responseError(403, 'Error while updating the profile picture')));
 }
--- post/removeUserFromRanking.php
+++ post/removeUserFromRanking.php
@@ -8,7 +8,8 @@ $token = getClientToken();
 $decoded = AUTH::decodeToken($token);
 $id = $decoded->data[1];
 try {
-    $database->insertUsersToRanking($validation->users, $validation->idRanking);
+    $user = $database->getUserByEmail($validation->email);
+    $database->deleteUserFromRanking($user['id'], $validation->idRanking);
 } catch (Exception $error) {
-    print_r(json_encode($database->responseError(403, 'Invalid data')));
+    print_r(json_encode($database->responseError(403, 'Error while updating the profile picture')));
 }
