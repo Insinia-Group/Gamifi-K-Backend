cd5bdd3b8d77dd5f26c865d1e26b84e153f0867c
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-12 13:51:56 +0200

    Cambiando funcionamiento para conseguir los Ids de los usuarios

--- post/addUsersToRanking.php
+++ post/addUsersToRanking.php
@@ -8,7 +8,12 @@ $token = getClientToken();
 $decoded = AUTH::decodeToken($token);
 $id = $decoded->data[1];
 try {
-    $database->insertUsersToRanking($validation->users, $validation->idRanking);
+    $users = [];
+    foreach ($validation->users as $email) {
+        $response = $database->getUserByEmail($email);
+        array_push($users, $response['id']);
+    };
+    $database->insertUsersToRanking($users, $validation->idRanking);
 } catch (Exception $error) {
     print_r(json_encode($database->responseError(403, 'Invalid data')));
 }
